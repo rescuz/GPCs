27677c4d2c2188618c73cdd3e84e101f0aa872af
rescuz <52475954+rescuz@users.noreply.github.com>
2022-07-18 11:19:47 +0800

    Update run.split_chr.sh

--- demo/run.split_chr.sh
+++ demo/run.split_chr.sh
@@ -2,7 +2,7 @@ dir=`pwd`
 if [ -e $dir/qsu.sh  ] ; then rm -f $dir/qsu.sh ; fi
 mkdir -p  {$dir/tmp,$dir/shell}
 ### the full file of variant #####
-file=/hwfssz8/MGI_RUIJIN/User/zengbing/job/disease/Hyperlipidemia/FH/s2.QC_variant/old/APOA5
+file=/path/to/bed_like_file
 dir2=$dir/test
 name=${file##*/}
 for i in {1..22} X Y M 
@@ -11,20 +11,20 @@ do
 	if [ -s  $dir/tmp/chr$i.${name}   ] 
 	then
 		if [ ! -d $dir2/chr$i ] ; then  mkdir -p $dir2/chr$i ; fi  
-		echo "sh /hwfssz8/MGI_RUIJIN/User/zengbing/job/disease/Hyperlipidemia/FH/s2.QC_variant/old/GPC/GPCs.sh \\
-		-v /hwfssz8/MGI_RUIJIN/User/zengbing/job/disease/Hyperlipidemia/FH/s2.QC_variant/old/tmp.vcf.list \\
+		echo "sh /*/GPC/GPCs.sh \\
+		-v /*/tmp.vcf.list \\
 		-b $dir/tmp/chr$i.${name} \\
 		-i 20 \\
 		-n ChinaMAP.GPC.$name.chr$i \\
 		-d  $dir2/chr$i \\
-		-V /hwfssz8/MGI_RUIJIN/Project/ChinaMAP/CohortAnalysis/phase1_10588/01.DATA/ \\
+		-V /path/to/database \\
 		-S /share/app/singularity-3.2.0/bin/singularity \\
 		-t 10 \\
 		-s 4 \\
 		-r 0.2 \\
-		-f /hwfssz8/MGI_RUIJIN/User/zengbing/job/disease/Hyperlipidemia/FH/s2.QC_variant/old/10588.phenotype \\
-		-T /hwfssz8/MGI_RUIJIN/User/zengbing/job/disease/Hyperlipidemia/FH/s2.QC_variant/old/GPC \\
-		-p /hwfssz8/MGI_RUIJIN/User/zengbing/job/disease/Hyperlipidemia/FH/s2.QC_variant/old/pheno.txt 
+		-f /path/to/phenotype_file \\
+		-T /path/to/result \\
+		-p /path/to/pheno.txt 
 		-M 1 \
 		-m 1 \
 		-N 1 
