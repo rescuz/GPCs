e9fd3bab6634ecef2b0671f01c5583e070236af5
rescuz <52475954+rescuz@users.noreply.github.com>
2022-07-19 09:21:52 +0800

    Update run.split_chr.sh

--- demo/run.split_chr.sh
+++ demo/run.split_chr.sh
@@ -11,23 +11,23 @@ do
 	if [ -s  $dir/tmp/chr$i.${name}   ] 
 	then
 		if [ ! -d $dir2/chr$i ] ; then  mkdir -p $dir2/chr$i ; fi  
-		echo "sh /*/GPC/GPCs.sh \\
+		echo -n "sh /*/GPC/GPCs.sh \\
 		-v /*/tmp.vcf.list \\
 		-b $dir/tmp/chr$i.${name} \\
 		-i 20 \\
 		-n ChinaMAP.GPC.$name.chr$i \\
 		-d  $dir2/chr$i \\
-		-V /path/to/database \\
+		-V /path/to/vcf \\
+		-a /path/to/database \\
 		-S /share/app/singularity-3.2.0/bin/singularity \\
 		-t 10 \\
 		-s 4 \\
 		-r 0.2 \\
 		-f /path/to/phenotype_file \\
 		-T /path/to/result \\
-		-p /path/to/pheno.txt 
-		-M 1 \
-		-m 1 \
-		-N 1 
+		-p /path/to/pheno.txt \\
+		-M 1 \\
+		-m 1 
 		echo "Still_waters_run_deep" > $dir/shell/ChinaMAP.GPC.$name.chr$i.sh.sign "  >$dir/shell/ChinaMAP.GPC.$name.chr$i.sh
 		### change according to your cluster management system   ####
 		echo "qsub -cwd -l vf=20g,p=20 -q mgi_ruijin.q -P MGI_RUIJIN -binding linear:20  $dir/shell/ChinaMAP.GPC.$name.chr$i.sh "  >>$dir/qsu.sh 
